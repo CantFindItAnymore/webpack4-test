554c843c09da0ee4739e7d66ea07b6a702a821e1
Ran Jun <ranjun@ybtcn.cn>
2020-09-14 15:04:06 +0800

    progress: devServer

---
+++ demo1/header.js
@@ -0,0 +1,6 @@
+export default function Header () {
+  var dom = document.getElementById('root')
+  const header = document.createElement('div')
+  header.innerHTML = 'header'
+  dom.append(header)
+}
---
+++ demo1/index.js
@@ -0,0 +1,10 @@
+// index.js
+
+// let dom = document.getElementById('root')
+// const header = document.createElement('div')
+// header.innerHTML = 'header'
+// dom.append(header)
+
+
+import Header from './header.js'
+new Header()
